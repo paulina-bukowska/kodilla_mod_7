7f43e5e9016d8a3e04bd8cbfc6bc3c14f324640a
paulawaster <pbukowska16@gmail.com>
2017-11-23 22:50:01 +0100

    7.2. Czym jest Stream? Zadanie: Niemutowalny użytkownik forum.

--- src/main/java/com/kodilla/stream/StreamMain.java
+++ src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,7 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
@@ -38,6 +36,7 @@ public class StreamMain {
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
+        System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
@@ -48,5 +47,8 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
